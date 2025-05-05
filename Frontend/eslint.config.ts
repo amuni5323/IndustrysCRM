8e5d15aa88e42ed75903a20a527048f1edd7ff8f
amuni5323 <amuni11.hamai@gmail.com>
2025-05-06 01:16:33 +0300

    done done

--- Frontend/eslint.config.ts
+++ Frontend/eslint.config.ts
@@ -1,3 +1,27 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     // Custom rules
+//     rules: {
+//       "@next/next/no-html-link-for-pages": "off",
+//     },
+//   },
+// ];
+
+// export default eslintConfig;
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -19,4 +43,23 @@ const eslintConfig = [
   },
 ];
 
+// If you want to apply the `ignoreDuringBuilds` config:
+eslintConfig.push({
+  overrides: [
+    {
+      files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+      rules: {
+        "eslint/ignoreDuringBuilds": ["warn", "error"]
+      }
+    },
+  ],
+});
+
 export default eslintConfig;
+
+// Module export for enabling the `ignoreDuringBuilds` config
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
